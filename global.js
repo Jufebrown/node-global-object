7a8899f50d8c1e4c079c2c99c258ec603ad84126
Jufebrown <jufebrown@gmail.com>
2017-03-03 09:32:02 -0600

    refactored with Joe's code

--- global.js
+++ global.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Node.js version:', 'v' + process.versions.node)
-console.log('V8 version:', process.versions.v8)
+// console.log('Node.js version:', 'v' + process.versions.node)
+// console.log('V8 version:', process.versions.v8)
+
+const {versions: {node, v8}} = process,
+      output = `Node.js version: ${node}\nV8 version: ${v8}`
+
+console.log(output)
